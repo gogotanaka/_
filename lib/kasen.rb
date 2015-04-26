c33a2cacb66b28fd7a878981182a2ef139f9e073
gogotanaka <mail@tanakakazuki.com>
2015-04-26 11:17:10 -0700

    Introduce Kasen::Context

--- lib/kasen.rb
+++ lib/kasen.rb
@@ -2,32 +2,53 @@ require "kasen/version"
 
 module Kernel
   def k
-    Kasen.new
+    Kasen::Context.new
   end
   alias _ k
 end
 
-class Kasen
-  # Disable all build-in methods
-  verbose, $VERBOSE = $VERBOSE, nil
-  begin
-    instance_methods(true).each { |meth| undef_method(meth) }
-    private_instance_methods(true).each { |meth| undef_method(meth) }
-  ensure
-    $VERBOSE = verbose
+module Kasen
+  class EmptyObject
+    verbose, $VERBOSE = $VERBOSE, nil
+    begin
+      instance_methods(true).each { |meth| undef_method(meth) }
+      private_instance_methods(true).each { |meth| undef_method(meth) }
+    ensure
+      $VERBOSE = verbose
+    end
   end
 
-  def initialize
-    @__kasen__ = Proc.new { |o| o }
+  class Context < EmptyObject
+    def initialize
+      @__kasen__ = Proc.new { |o| o }
+    end
+
+    def to_proc
+      @__kasen__
+    end
+
+    def method_missing(name, *args, &block)
+      __kasen__ = @__kasen__
+      @__kasen__ = Proc.new { |o| (__kasen__.(o)).send(name, *args, &block) }
+      self
+    end
   end
 
-  def to_proc
-    @__kasen__
-  end
+  class ContextGenerator < EmptyObject
+    def initialize
+    end
 
-  def method_missing(name, *args, &block)
-    __kasen__ = @__kasen__
-    @__kasen__ = Proc.new { |o| (__kasen__.(o)).send(name, *args, &block) }
-    self
+    def method_missing(name, *args, &block)
+      Kasen::Context.new.send(name, *args, &block)
+    end
+  end
+end
+
+if defined?(IRB::Context)
+  class IRB::Context
+    def set_last_value(value)
+      @last_value = value
+      @workspace.evaluate self, "_ = Kasen::ContextGenerator.new"
+    end
   end
 end
